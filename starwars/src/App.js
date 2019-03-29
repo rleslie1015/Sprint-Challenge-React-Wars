fd826b52fcee1ed48a761c171d0aaf9d71cc5873
lrod <rleslie1015@gmail.com>
2019-03-29 12:36:13 -0700

    console log props finally givews me an array of objects

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import CharactersList from './components/CharactersList';
+
 class App extends Component {
   constructor() {
     super();
@@ -33,6 +35,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <CharactersList charactors={this.state.starwarsChars} />
+
+      
       </div>
     );
   }
---
+++ starwars/src/components/CharactersList.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CharactersList = props => {
+  console.log(props);
+  return <div>OUr list  </div>
+}
+export default CharactersList;
+
